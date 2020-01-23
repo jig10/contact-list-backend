fc8c3ecd293b73e6ba940b14e7d4d917f09cc0f0
jig10 <jigodoyrubin@gmail.com>
2020-01-23 00:07:13 +0000

    first commit

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate
 from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
-from models import db
+from models import db, Person
 #from models import Person
 
 app = Flask(__name__)
@@ -27,7 +27,55 @@ def handle_invalid_usage(error):
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+@app.route('/person', methods=['GET', 'POST']) 
+def handle_query():
+     if request.method =='GET':
+         people_query = Person.query.all()
+         all_people = list(map(lambda x: x.serialize(), people_query))
+         return jsonify(all_people), 200
+     if request.method =='POST':
 
+        #user1 = Person(username="jim godoy", email="my_xuper@email.com")
+        
+        body = request.get_json()
+        user1 = Person(full_name=body.get('full_name'), username=body.get('username'), email=body.get('email'), phone=body.get('phone'), address=body.get('address'))
+        db.session.add(user1)
+        db.session.commit()
+        return jsonify(user1.id), 200
+@app.route('/person/<int:person_id>', methods=['PUT', 'GET', 'DELETE'])
+def get_single_person(person_id):
+    """
+    Single person
+    """
+    # PUT request
+    if request.method == 'PUT':
+        body = request.get_json()
+        if body is None:
+            raise APIException("You need to specify the request body as a json object", status_code=400)
+        user1 = Person.query.get(person_id)
+        if user1 is None:
+            raise APIException('User not found', status_code=404)
+        if "username" in body:
+            user1.username = body["username"]
+        if "email" in body:
+            user1.email = body["email"]
+        db.session.commit()
+        return jsonify(user1.serialize()), 200
+    # GET request
+    if request.method == 'GET':
+        user1 = Person.query.get(person_id)
+        if user1 is None:
+            raise APIException('User not found', status_code=404)
+        return jsonify(user1.serialize()), 200
+    # DELETE request
+    if request.method == 'DELETE':
+        user1 = Person.query.get(person_id)
+        if user1 is None:
+            raise APIException('User not found', status_code=404)
+        db.session.delete(user1)
+        db.session.commit()
+        return "ok", 200
+    return "Invalid Method", 404
 @app.route('/hello', methods=['POST', 'GET'])
 def handle_hello():
 
